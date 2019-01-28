e67570687d61e2b18c8b92c987b71d1056683bd2
Dennis Zelada <denniszelada@gmail.com>
2019-01-28 13:41:44 +0000

    wire resolver for menu_items

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,8 +1,11 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
+  alias PlateSlate.{Menu, Repo}
 
   query do
-    field :menu_items, list_of(:menu_item)
+    field :menu_items, list_of(:menu_item) do
+      resolve fn _, _, _ -> {:ok, Repo.all(Menu.Item)}
+    end
   end
 
   object :menu_item do
